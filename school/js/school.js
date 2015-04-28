465b9a45f698e45310b78fae27aa9356671aace6
Vinso014 <vinso014@umn.edu>
2015-04-27 22:22:49 -0500

    uploaded completed site

--- school/js/school.js
+++ school/js/school.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-	
+
+	var scrolled=0; // scroll starting point
+
+	$(".down").on("click" ,function(){
+        scrolled=scrolled+1100; // 1050 is the amount of pixels that is scrolled down
+        $("body").animate({
+			scrollTop:  scrolled
+		});
+	});
+
+    $(".up").on("click" ,function(){
+		scrolled=scrolled-1100; // 1050 is the amount of pixels that is scrolled up
+		$("body").animate({
+			scrollTop:  scrolled
+		});
+	});
+
 });
